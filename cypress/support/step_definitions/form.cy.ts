0f138d298156fa2a1b2eef40149cc0bf05326151
aldimhernandez <marialdanahz@gmail.com>
2025-03-24 13:44:29 -0300

    refactor: :sparkles: POM was implemented
    Each of the steps is replaced by class methods
    
    https://github.com/aldimhernandez/cypress-e2e-testing/issues/11

--- cypress/support/step_definitions/form.cy.ts
+++ cypress/support/step_definitions/form.cy.ts
@@ -1,6 +1,6 @@
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 import { homePage } from "../../pages/form/HomePage";
-import { formPage } from "../../pages/form/FormPage";
+import { formPage, practiceFormPage } from "../../pages/form/FormPage";
 
 //* Background
 Given('I visit the demoqa site', () => {
@@ -12,76 +12,62 @@ When('I navigate to the form section', () => {
 });
 
 When('I click on the practice form button', () => {
-    formPage.validateUrl();
-    cy.contains('Please select an item from left to start practice.').should('be.visible');
-    cy.contains('.element-list', 'Practice Form').should('be.visible').click();
+    formPage.pageRedirectValidation();
+    formPage.navigateToPracticeForm();
 });
 
 Then('I see the empty form', () => {
-    cy.url().should('eq', 'https://demoqa.com/automation-practice-form');
-    cy.get('h1').should('have.text', 'Practice Form').and('be.visible');
-    cy.get('h5').should('have.text', 'Student Registration Form').and('be.visible');
-    cy.get('form').within(() => {
-        cy.get('input').should('have.length', 15).and('be.empty');
-    });
+    practiceFormPage.pageRedirectValidation();
 });
 
 //* Scenario: Fill out the form with valid data
 When('I enter my personal details {string} {string} {string}', (name: string, lastName: string, email: string) => {
-    cy.get('#firstName').type(name);
-    cy.get('#lastName').type(lastName);
-    cy.get('#userEmail').type(email);
+    practiceFormPage.fillForm(name, lastName, email);
 });
 
 When('I choose {string} as my gender', (gender: string) => {
-    cy.get('[id^=gender-radio-]').next('label').contains(gender).click();
+    practiceFormPage.selectGender(gender);
 });
 
 When('I enter {} as my mobile number', (mobile: number) => {
-    cy.get('#userNumber').type(mobile.toString());
+    practiceFormPage.enterMobileNumber(mobile);
 });
 
 When('I enter {string} as my date of birth', (dob: string) => {
-    cy.get('#dateOfBirthInput').click().type(`{selectall}${dob}{enter}`);
+    practiceFormPage.enterDateOfBirth(dob);
 });
 
 When('I fill out the subject field {string}', (subject: string) => {
-    cy.get('#subjectsInput').type(subject).type('{enter}');
+    practiceFormPage.fillOutSubjectField(subject);
 });
 
 When('I select the hobbies field {string}', (hobbies: string) => {
-    const hobbiesArray = hobbies.split(',').map(h => h.trim()); // Convierte el string en array
-    hobbiesArray.forEach((hobby: string) => {
-        cy.get('[id^=hobbies-checkbox-]').next('label').contains(hobby).click();
-    });
+    practiceFormPage.selectHobbies(hobbies);
 });
 
-When('I upload the picture file {string}', (picture) => {
-
+When('I upload the picture file {string}', (picture: string) => {
+    practiceFormPage.uploadPicture(picture);
 });
 
 When('I fill out the current address field {string}', (address: string) => {
-    cy.get('#currentAddress').type(address);
+    practiceFormPage.fillOutCurrentAddress(address);
 });
 
 When('I select {string} and {string}', (state: string, city: string) => {
-    cy.get('#state').type(`${state}{enter}`);
-    cy.get('#city').type(`${city}{enter}`);
+    practiceFormPage.selectStateAndCity(state, city);
 });
 
 When('I click on the submit button', () => {
-    cy.get('#submit').click();
+    practiceFormPage.submitForm();
 });
 
 Then('I should see the form submission message', () => {
-    cy.get('.modal-header').should('have.text', 'Thanks for submitting the form').and('be.visible');
+    practiceFormPage.formSubmissionValidation();
 });
 
 //* Scenario: Fill out the form with invalid data
 When('I leave the gender field empty', () => { });
 
 Then('I should see the form error message', () => {
-    cy.get('#userEmail').should('have.css', 'border-color', 'rgb(220, 53, 69)');
-    cy.get('#userNumber').should('have.css', 'border-color', 'rgb(220, 53, 69)');
-    cy.contains('Thanks for submitting the form').should('not.exist');
+    practiceFormPage.formErrorMessageValidation();
 });
