a6041f8808b132a6fb5a7f0e03e72440f077c041
aldimhernandez <marialdanahz@gmail.com>
2025-03-24 13:50:07 -0300

    refactor: :art: Improve code structure
    refactor with best ts practice
    
    https://github.com/aldimhernandez/cypress-e2e-testing/issues/11

--- cypress/pages/form/HomePage.ts
+++ cypress/pages/form/HomePage.ts
@@ -1,19 +1,17 @@
 import BasePage from "../BasePage";
 
-
 class HomePage extends BasePage {
-    private readonly navToFormSelector: string;
-    private readonly formCardText: string;
+    private readonly navToFormSelector: string = '[class="card mt-4 top-card"]'
 
     constructor() {
         super('DEMOQA', '/');
-        this.navToFormSelector = '[class="card mt-4 top-card"]';
-        this.formCardText = 'Forms';
     }
 
     navigateToFormSection() {
-        this.clickElementByText(this.formCardText, this.navToFormSelector);
+        this.clickElementByText(HomePage.FORM_CARD_TEXT, this.navToFormSelector);
     }
+
+    static readonly FORM_CARD_TEXT: string = 'Forms';
 }
 
 export const homePage = new HomePage();
